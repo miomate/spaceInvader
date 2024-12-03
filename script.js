53d4d9e8876779f4b68beec23815126bee46323b
miomate <mrspec@outlook.com>
2024-12-03 09:14:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const scoreElement = document.querySelector('.score')
+const healthElement = document.querySelector('.health')
+const gameArea = document.querySelector('gameArea')
+const width = 15;
+
+for (let i = 0; i < width * width; i++) {
+    const element = array[index];
+    
+}
